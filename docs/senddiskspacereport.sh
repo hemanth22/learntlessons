359e57c57868d7e9c2631ae3d26f6158ae3ef289
Hemanth B <hemanth22hemu@gmail.com>
2021-10-30 20:51:12 +0530

    Update senddiskspacereport.sh

--- docs/senddiskspacereport.sh
+++ docs/senddiskspacereport.sh
@@ -1,2 +1,2 @@
 /root/checkspace.sh | awk -f stat.awk > report$(date +%Y%m%d).html
-cat mailheaderautomation report$(date +%Y%m%d).html | sendmail -t
+cat mailheader report$(date +%Y%m%d).html | sendmail -t
