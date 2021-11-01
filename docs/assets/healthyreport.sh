0d7983fd73867f59893d36ed016011cbc65a3fdf
Hemanth B <hemanth22hemu@gmail.com>
2021-11-01 20:06:49 +0530

    Update healthyreport.sh

--- docs/assets/healthyreport.sh
+++ docs/assets/healthyreport.sh
@@ -1,4 +1,12 @@
 /root/checkspace.sh | awk -f help1.awk > reporthealthcheck$(date +%Y%m%d).html
 /root/healthcheck.sh | awk -f help2.awk >> reporthealthcheck$(date +%Y%m%d).html
 chmod 777 reporthealthcheck$(date +%Y%m%d).html
+sed -i -e 's/\x1b//g' reporthealthcheck20211031.html
+sed -i -e 's/;//g' reporthealthcheck20211031.html
+sed -i -e 's/\[//g' reporthealthcheck20211031.html
+sed -i -e 's/<pre>134m\*0m Process Healthcheck 134m\*0m<\/pre>/<pre>* Process Healthcheck *<\/pre>/g' reporthealthcheck20211031.html
+sed -i -e 's/<pre>134m/<pre bgcolor=blue>/g' reporthealthcheck20211031.html
+sed -i -e 's/0m<\/pre>/<\/pre>/g' reporthealthcheck20211031.html
+sed -i -e 's/<pre>032m/<pre bgcolor=green>/g' reporthealthcheck20211031.html
+sed -i -e 's/<pre>031m/<pre bgcolor=green>/g' reporthealthcheck20211031.html
 python3 mailreporting.py
